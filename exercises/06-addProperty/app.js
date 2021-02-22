3255a21c8f9335309f4e7fa7bc143e9255859947
Edventures <edufg13@gmail.com>
2021-02-22 18:26:25 +0000

    estoy en p12

--- exercises/06-addProperty/app.js
+++ exercises/06-addProperty/app.js
@@ -1,3 +1,8 @@
 function addProperty(obj, key) {
   // your code here
+  obj[key] = "value";
+  return obj;
 }
+var myObj = {};
+addProperty(myObj, 'myProperty');
+console.log(myObj.myProperty); // --> true
